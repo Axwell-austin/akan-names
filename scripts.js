dfe7594ae57b090e2151ae0b75e34f8d494afa03
Austin-axwell <austin.onyango@student.moringaschool.com>
2021-07-17 20:59:50 +0300

    Added the statements of the JS with the arrays that get elements of the weekdays and returns the values

--- scripts.js
+++ scripts.js
@@ -1,30 +1,105 @@
 var Calculator = function() {
     var date = parseInt(
-        document.getElementById("Form").elements.namedItem("date").value
+    document.getElementById("Form").elements.namedItem("date").value
+    );
+    var month = parseInt(
+        document.getElementById("Form").elements.namedItem("month").value
         );
-        var month = parseInt(
-            document.getElementById("Form").elements.namedItem("month").value
-          );
-          var year = parseInt(
-            document.getElementById("Form").elements.namedItem("year").value
-          );
-          var gender = document.getElementById("Form").elements.namedItem("gender")
-            .value;
-            var dateOfBirth = new Date(year + "/" + month + "/" + date);
-            var weekDay = dateOfBirth.getDay();
+        var year = parseInt(
+        document.getElementById("Form").elements.namedItem("year").value
+        );
+        var gender = document.getElementById("Form").elements.namedItem("gender")
+        .value;
+        var dateOfBirth = new Date(year + "/" + month + "/" + date);
+        var weekDay = dateOfBirth.getDay();
 
-            if (month < 1 || month > 12 || (month == 2 && date > 29)) {
-                return (document.getElementById("alert2").innerHTML =
-                  "INPUT A VALID MONTH!");
-              } else if (date < 1 || date > 31) {
-                return (document.getElementById("alert2").innerHTML =
-                  "INPUT A VALID DATE!");
-              } else if (year === NaN || year < 1900 || year > 2025) {
-                document.getElementById("alert2").innerHTML =
-                  "INPUT A VALID YEAR!";
-              }
-              var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+        if (month < 1 || month > 12 || (month == 2 && date > 29)) {
+            return (document.getElementById("alert2").innerHTML =
+                "INPUT A VALID MONTH!");
+            } else if (date < 1 || date > 31) {
+            return (document.getElementById("alert2").innerHTML =
+                "INPUT A VALID DATE!");
+            } else if (year === NaN || year < 1900 || year > 2025) {
+            document.getElementById("alert2").innerHTML =
+                "INPUT A VALID YEAR!";
+            }
+            var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-              var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+            var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+        
+            var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        if (weekDay === 0 && gender === "male") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanMale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 1 && gender === "male") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanMale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 2 && gender === "male") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanMale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 3 && gender === "male") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanMale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 4 && gender === "male") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanMale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 5 && gender === "male") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanMale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 6 && gender === "male") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanMale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        }
+    
+        if (weekDay === 0 && gender === "female") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanFemale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 1 && gender === "female") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanFemale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 2 && gender === "female") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanFemale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 3 && gender === "female") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanFemale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 4 && gender === "female") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanFemale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 5 && gender === "female") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanFemale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+        } else if (weekDay === 6 && gender === "female") {
+        return (document.getElementById("alert").innerHTML =
+            "Congratulations! Your Akan name is  " +
+            akanFemale[weekDay] +
+            ("  .You were born on a  " + daysOfTheWeek[weekDay]));
+    }
+}
             
-              var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
