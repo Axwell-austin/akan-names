f184630d99338dd62ac0bf8dc0098b8f05298902
Austin-axwell <austin.onyango@student.moringaschool.com>
2021-07-17 20:35:38 +0300

    Added the get alement by specific id of the year, month and gender

--- scripts.js
+++ scripts.js
@@ -2,3 +2,11 @@ var Calculator = function() {
     var date = parseInt(
         document.getElementById("Form").elements.namedItem("date").value
         );
+        var month = parseInt(
+            document.getElementById("Form").elements.namedItem("month").value
+          );
+          var year = parseInt(
+            document.getElementById("Form").elements.namedItem("year").value
+          );
+          var gender = document.getElementById("Form").elements.namedItem("gender")
+            .value;
